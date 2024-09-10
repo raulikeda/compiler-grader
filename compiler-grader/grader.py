177c88ee78c2a09a429216fd6ab751c48e401163
Raul Ikeda <RaulIGS@insper.edu.br>
2024-09-10 16:59:28 -0500

    initial version funcional without tests

--- compiler-grader/grader.py
+++ compiler-grader/grader.py
@@ -2,20 +2,54 @@
 # See the book for more details
 import subprocess
 import os
+import sys
 
-def grade_version(command: str, version: str, direct_input: bool = True, maxtime: int = 10, fail_first: bool = False) -> str:
 
+def grade_version(file: str, version: str, **kwargs) -> str:
+
+    # Get the intepreter
+    interpreter = sys.executable
+
+    # check if version patten is correct vn.n
+    if not version[0] == "v" and version[2] == ".":
+        raise Exception("Invalid version pattern")
+
+    # Check if the file exists
+    if not os.path.exists(file):
+        raise Exception("File not found")
+
+    # Check if the file is a python file
+    if not file.endswith(".py"):
+        raise Exception("File is not a python file")
+
+    direct_input = True
+    if int(version[1]) >= 2:
+        direct_input = False
+
+    errors = run_test_files(
+        f"{interpreter} {file}", version, direct_input=direct_input, **kwargs
+    )
+
+    for error in errors:
+        print(error.replace("````", ""))
+
+
+def run_test_files(
+    command: str,
+    version: str,
+    direct_input: bool = True,
+    maxtime: int = 10,
+    fail_first: bool = False,
+) -> str:
 
     # Get the current file path
     current_file_path = __file__
 
     # Get the directory of the current file
     current_directory = os.path.dirname(current_file_path)
-    
-    print(current_directory)
 
     # List all the files in the tests/version directory
-    files = os.listdir(f'{current_directory}/tests/{version}')
+    files = os.listdir(f"{current_directory}/tests/{version}")
 
     # sort the list to get right test order
     files.sort()
@@ -25,36 +59,44 @@ def grade_version(command: str, version: str, direct_input: bool = True, maxtime
     for file in files:
 
         # if file starts with letter t
-        if file[0] != 't':
-                
+        if file[0] == "t":
+
             # Get the path to the file
-            path = f'{current_directory}/tests/{version}/{file}'
+            path = f"{current_directory}/tests/{version}/{file}"
 
             case = int(file[1:4])
 
             # Read file content
             if direct_input:
-                with open(path, 'r') as f:
+                with open(path, "r") as f:
                     content = f.read()
             else:
-                # Get absolute path for the file
-                path = os.path.abspath(path)
-                # Content will be the file with path as argument
-                content = f'{path}/{file}'
+                content = path
 
             # Read the solution file starting with letter s
-            with open(f'{current_directory}/tests/{version}/s{case:03d}.txt', 'r') as f:
+            with open(f"{current_directory}/tests/{version}/s{case:03d}.txt", "r") as f:
                 solution = f.read()
 
             try:
                 # Run the command with content as argument and get the output
-                output = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, input=content.encode('utf-8'), timeout=maxtime)
+                args = command.split(" ")
+                args.append(content)
+                output = subprocess.run(
+                    args,
+                    # shell=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    timeout=maxtime,
+                )
 
+                result = output.stdout.decode("utf-8").strip()
+
+            # If the process takes too long to execute
             except subprocess.TimeoutExpired:
 
                 report = f"Test {case}: FAIL\n"
                 report += f"Input:\n````"
-                report += get_test_content(path, direct_input) + "\n"
+                report += get_test_content(content, direct_input) + "\n"
                 report += "````\n\n"
                 report += f"Error: Timeout, took too long to execute. Timeout: {maxtime} seconds\n"
 
@@ -62,15 +104,16 @@ def grade_version(command: str, version: str, direct_input: bool = True, maxtime
 
             # Check if the return code is 0 (success)
             if output.returncode == 0:
+
                 # Compare the output with the solution
-                if output != solution:
-                    
+                if output.stdout.decode("utf-8").strip() != solution.strip():
+
                     report = f"Test {case}: FAIL\n"
                     report += f"Input:\n````"
-                    report += get_test_content(path, direct_input) + "\n"
+                    report += get_test_content(content, direct_input) + "\n"
                     report += "````\n\n"
                     report += f"Expected output:\n````\n{solution}\n````\n"
-                    report += f"Actual output:\n````\n{output}\n````\n"
+                    report += f"Actual output:\n````\n{result}\n````\n"
 
                     # If fail_first is True, raise an exception immediately
                     if fail_first:
@@ -79,16 +122,18 @@ def grade_version(command: str, version: str, direct_input: bool = True, maxtime
                     # Add the report to the errors list
                     errors.append(report)
 
-            else: # If the return code is not 0 (error)
+            else:  # If the return code is not 0 (error)
                 # Compare the output with the error tag
-                if solution != '[ERROR]':
+                if solution != "[ERROR]":
+
+                    print(solution)
 
                     report = f"Test {case}: FAIL\n"
                     report += f"Input:\n````"
-                    report += get_test_content(path, direct_input) + "\n"
+                    report += get_test_content(content, direct_input) + "\n"
                     report += "````\n\n"
-                    report += f"Expected output:\n````\nError\n````\n"
-                    report += f"Actual output:\n````\n{output}\n````\n"
+                    report += f"Expected output:\n````\{solution}\n````\n"
+                    report += f"Actual output:\n````\nError\n````\n"
 
                     # If fail_first is True, raise an exception immediately
                     if fail_first:
@@ -96,13 +141,14 @@ def grade_version(command: str, version: str, direct_input: bool = True, maxtime
 
                     # Add the report to the errors list
                     errors.append(report)
+
+    return errors
 
 
 # Get the test content from a file or direct input
 def get_test_content(test: str, direct_input: bool = True) -> str:
-    if direct_input: # if direct input, test will be the content
+    if direct_input:  # if direct input, test will be the content
         return test
-    else: # if not, read the file content
-        with open(test, 'r') as f:
+    else:  # if not, read the file content
+        with open(test, "r") as f:
             return f.read()
-
--- testcode.py
+++ testcode.py
@@ -1,1 +1,10 @@
-print(3)
+import sys
+import time
+
+import sys
+
+code = sys.argv[1]
+if "t002.txt" in code:
+    raise Exception("!")
+else:
+    print(3)
