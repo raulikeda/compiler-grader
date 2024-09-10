a0848b6ee51fc00a20fd51ddb3344e5a3fa058f7
Raul Ikeda <rauligs@insper.edu.br>
2024-09-10 10:04:09 -0500

    initial files

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="compiler-grader",
+    version="0.1.0",
+    author="Raul Ikeda",
+    author_email="rauligs@insper.edu.br",
+    description="Lib for autograding the compiler project from the book",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rikeda/compiler-grader",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        # Add dependencies here
+    ],
+)
