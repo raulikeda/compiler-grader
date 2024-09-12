187070958407694a2170a20bb48bf1fae1d3332e
Raul Ikeda <RaulIGS@insper.edu.br>
2024-09-12 13:37:09 -0500

    test

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="compiler_grader",
+    name="compiler-grader",
     version="0.1.0",
     author="Raul Ikeda",
     author_email="rauligs@insper.edu.br",
